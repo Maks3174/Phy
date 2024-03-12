9bb7c84907f20f20b4e54ddb72c57ae9541f36d4
Maks3174 <matviieshin@gmail.com>
2024-03-12 20:28:16 +0200

    task5

--- less.py
+++ less.py
@@ -1,12 +1,28 @@
-class Car:
-    def __init__(self, brand, model, year):
-        self.brand = brand
-        self.model = model
-        self.year = year
+class BankAccount:
+    def __init__(self, owner, balance=0):
+        self.owner = owner
+        self.balance = balance
 
-    def start_engine(self):
-        print("Двигун запущено.")
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            print(f"Внесено {amount} грошей. Новий баланс: {self.balance}")
+        else:
+            print("Недійсна сума для внесення.")
+
+    def withdraw(self, amount):
+        if amount > 0:
+            if self.balance >= amount:
+                self.balance -= amount
+                print(f"Знято {amount} грошей. Новий баланс: {self.balance}")
+            else:
+                print("Недостатньо коштів на рахунку.")
+        else:
+            print("Недійсна сума для зняття.")
 
 
-car1 = Car("Mercedes-Benz", "W220", 2005)
-car1.start_engine()
+account1 = BankAccount("Іван Петрович")
+account1.deposit(100)
+account1.withdraw(50)
+account1.withdraw(80)
+account1.deposit(-20)
