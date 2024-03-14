12478f0627d0989284491b2564ffdc3f2092b3a8
Maks3174 <matviieshin@gmail.com>
2024-03-14 18:18:45 +0200

    Hm2

--- Homework.py
+++ Homework.py
@@ -1,24 +1,24 @@
-class Person:
-    def __init__(self, full_name, date_of_birth, phone_number, city, country, home_address):
-        self.full_name = full_name
-        self.date_of_birth = date_of_birth
-        self.phone_number = phone_number
-        self.city = city
+class City:
+    def __init__(self, name, region, country, population, postal_code, phone_code):
+        self.name = name
+        self.region = region
         self.country = country
-        self.home_address = home_address
+        self.population = population
+        self.postal_code = postal_code
+        self.phone_code = phone_code
 
     def __str__(self):
-        return f"Full Name: {self.full_name}\nDate of Birth: {self.date_of_birth}\nPhone Number: {self.phone_number}\nCity: {self.city}\nCountry: {self.country}\nHome Address: {self.home_address}"
+        return f"City: {self.name}\nRegion: {self.region}\nCountry: {self.country}\nPopulation: {self.population}\nPostal Code: {self.postal_code}\nPhone Code: {self.phone_code}"
 
-    def update_phone_number(self, new_phone_number):
-        self.phone_number = new_phone_number
-        print("Phone number updated.")
+    def update_population(self, new_population):
+        self.population = new_population
+        print("Population updated.")
 
 
-person1 = Person("John Doe", "01-01-1990", "+123456789", "New York", "USA", "123 Main St")
-print("Person Information:")
-print(person1)
+city1 = City("New York", "New York", "USA", 8623000, "10001", "212")
+print("City Information:")
+print(city1)
 
-person1.update_phone_number("+987654321")
-print("\nUpdated Person Information:")
-print(person1)
+city1.update_population(9000000)
+print("\nUpdated City Information:")
+print(city1)
