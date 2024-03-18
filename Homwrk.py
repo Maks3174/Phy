e0849482f1032be7a378f9fcafbdcf99e6164b7a
Maks3174 <matviieshin@gmail.com>
2024-03-18 22:56:38 +0200

    task4

--- Homwrk.py
+++ Homwrk.py
@@ -1,3 +1,5 @@
+
+
 #4
 class Watch:
     def __init__(self, model_name, manufacturer, year, price, type_of_watch):
@@ -40,3 +42,47 @@ print(watch1 >= watch2)
 
 print(watch1)
 print(watch2)
+
+
+#5
+class Website:
+    def __init__(self, name, address, description):
+        self.name = name
+        self.address = address
+        self.description = description
+
+    def __str__(self):
+        return f"Назва: {self.name}, Адреса: {self.address}, Опис: {self.description}"
+
+    def __eq__(self, other):
+        return self.name == other.name and self.address == other.address and self.description == other.description
+
+    def __lt__(self, other):
+        return self.name < other.name
+
+    def __le__(self, other):
+        return self.name <= other.name
+
+    def __gt__(self, other):
+        return self.name > other.name
+
+    def __ge__(self, other):
+        return self.name >= other.name
+
+    def __ne__(self, other):
+        return self.name != other.name
+
+website1 = Website("Google", "https://www.google.com", "Пошукова система та інтернет-компанія")
+website2 = Website("Facebook", "https://www.facebook.com", "Соціальна мережа")
+
+
+print(website1 < website2)
+print(website1 <= website2)
+print(website1 == website2)
+print(website1 != website2)
+print(website1 > website2)
+print(website1 >= website2)
+
+
+print(website1)
+print(website2)
