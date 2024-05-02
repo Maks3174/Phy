d3657c9ff0d5c9f950a438bf3f1c463933226a6c
Maks3174 <matviieshin@gmail.com>
2024-05-02 20:42:19 +0300

    client_task1

--- Homework.py
+++ Homework.py
@@ -1,1 +1,23 @@
-class Country:
+import socket
+#1
+def main():
+    server_host = "127.0.0.1"
+    server_port = 12345
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((server_host, server_port))
+    print(f"З'єднання з сервером {server_host}:{server_port} встановлено")
+
+    while True:
+        message = input("Введіть повідомлення: ")
+        client_socket.send(message.encode('utf-8'))
+
+        response = client_socket.recv(1024).decode('utf-8')
+        print("Отримано відповідь від сервера:", response)
+
+    client_socket.close()
+
+if __name__ == "__main__":
+    main()
+
+
